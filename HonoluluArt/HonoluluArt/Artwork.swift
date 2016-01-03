0e28c2282854a954c7a1c1caeb85dddbbba6b5f8
Zachary Browne <browne.zachary@gmail.com>
2016-01-03 14:14:22 -0800

    tutorial work through the first "adding an artwork" part

---
+++ HonoluluArt/HonoluluArt/Artwork.swift
@@ -0,0 +1,30 @@
+//
+//  Artwork.swift
+//  HonoluluArt
+//
+//  Created by Zachary Browne on 1/3/16.
+//  Copyright © 2016 zbrowne. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Artwork: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+}
--- HonoluluArt/HonoluluArt/ViewController.swift
+++ HonoluluArt/HonoluluArt/ViewController.swift
@@ -7,17 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(initialLocation)
+        
+        // show artwork on map
+        let artwork = Artwork(title: "King David Kalakaua",
+            locationName: "Waikiki Gateway Park",
+            discipline: "Sculpture",
+            coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        
+        mapView.addAnnotation(artwork)
     }
 
 
